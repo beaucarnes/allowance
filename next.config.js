31e25d87fde367b7a17ea45f439d5dee90357e5a
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2024-12-05 14:49:48 -0500

    change build directory

--- next.config.js
+++ next.config.js
@@ -1,27 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
-  },
   experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
     serverActions: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/kids/:slug*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0',
-          },
-        ],
-      },
-    ]
+  images: {
+    unoptimized: true,
   },
+  output: 'standalone',
+  distDir: 'dist'
 }
 
 module.exports = nextConfig 
