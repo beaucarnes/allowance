1b0b3ddc5dcbc0d4f957d478f71c86354af0e453
Beau Carnes <1513130+beaucarnes@users.noreply.github.com>
2024-12-05 21:41:28 -0500

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import NavBar from './components/NavBar'
 import './globals.css'
 
+export const metadata = {
+  title: 'Track Allowance',
+  description: "The modern way to manage your children's allowance",
+  icons: {
+    icon: '/favicon.ico',
+    // You can also add different sizes if you want
+    // apple: '/apple-icon.png',
+    // shortcut: '/favicon-16x16.png',
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -8,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head />
       <body>
         <NavBar />
         <main className="max-w-6xl mx-auto px-4 py-8">
